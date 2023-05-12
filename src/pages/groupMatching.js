7a3cb32ada0ae759e5a225c67f5d35a45330af01
Yongseok-Lee <49047536+Yongseok-Lee@users.noreply.github.com>
2023-05-13 00:31:52 +0900

    Delete groupMatching.js

--- src/pages/groupMatching.js
+++
@@ -1,9 +0,0 @@
-import { sql } from '@velo/wix-data-sql-backend';
-import { groupMatching } from 'backend/groupMatchingModule'
-
-$w.onReady(function () {
-	$w('#button').onClick(async () => {
-		$w('#responseText').text = await sql("SELECT 'hello, world!';");
-		$w('#responseText').show();
-	});
-});
