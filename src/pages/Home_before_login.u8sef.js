ce5ad7f70b2c344a4e38ec7396f4037755c5b214
고영광 <70497430+readable-ko@users.noreply.github.com>
2023-05-12 17:23:17 +0900

    Update Home_before_login.u8sef.js

--- src/pages/Home_before_login.u8sef.js
+++ src/pages/Home_before_login.u8sef.js
@@ -1,10 +1,12 @@
 // API Reference: https://www.wix.com/velo/reference/api-overview/introduction
 // “Hello, World!” Example: https://learn-code.wix.com/en/article/1-hello-world
 
+import wixUsers from 'wix-users';
 import wixLocation from 'wix-location';
 
 $w.onReady(function () {
-  if (wixUsers.currentUser.loggedIn) {
+  wixUsers.onLogin(function (user) {
     wixLocation.to("/Home");
-  }
+  });
 });
+
