2b396a1796f39f147d15014d3b705bb890dd288f
고영광 <70497430+readable-ko@users.noreply.github.com>
2023-05-14 16:17:57 +0900

    Update SignUp.buek9.js

--- src/pages/SignUp.buek9.js
+++ src/pages/SignUp.buek9.js
@@ -1,10 +1,29 @@
 // API Reference: https://www.wix.com/velo/reference/api-overview/introduction
 // “Hello, World!” Example: https://learn-code.wix.com/en/article/1-hello-world
+import { authentication } from 'wix-members';
 
 $w.onReady(function () {
-    // Write your JavaScript here
+    $w.("#button1").onClick(() => {
+        console.log('TEST IS DOING WELL!!');
+    });
+//     authentication.register(email, password, options)
+//         .then((registrationResult) => {
+//         const status = registrationResult.status;
 
-    // To select an element by ID use: $w('#elementID')
-
-    // Click 'Preview' to run your code
+//         if (status === "PENDING") {
+//             // When the site is configured for manual approval,
+//             // status is "PENDING" and approvalToken is returned.
+//             const approvalToken = registrationResult.approvalToken;
+//             console.log('Member registered and waiting for approval:', registrationResult);
+//         } else {
+//             // When the site is configured for automatic approval,
+//             // status is "ACTIVE" and the member is approved and logged in.
+//             // To prevent logging in the member automatically,
+//             // use the backendend function: wix-members-backend.authentication.register()
+//             console.log('Member registered and logged in:', registrationResult);
+//         }
+//     })
+//         .catch((error) => {
+//         console.error(error);
+//     });
 });
