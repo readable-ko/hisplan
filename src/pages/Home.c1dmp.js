3d7d83e176471c626422d665b7e4835d71baf87b
고영광 <70497430+readable-ko@users.noreply.github.com>
2023-05-14 16:22:59 +0900

    Update Home.c1dmp.js

--- src/pages/Home.c1dmp.js
+++ src/pages/Home.c1dmp.js
@@ -1,19 +1,10 @@
 // API Reference: https://www.wix.com/velo/reference/api-overview/introduction
 // “Hello, World!” Example: https://learn-code.wix.com/en/article/1-hello-world
 
-// $w.onReady(function () {
-    // Write your JavaScript here
-
-    // To select an element by ID use: $w('#elementID')
-
-    // Click 'Preview' to run your code
-// });
-
-import wixUsers from 'wix-users';
-import wixLocation from 'wix-location';
-
 $w.onReady(function () {
-  wixUsers.register.onSuccess(function (user) {
-    wixLocation.to("https://www.naver.com/");
-  });
+//     Write your JavaScript here
+
+//     To select an element by ID use: $w('#elementID')
+
+//     Click 'Preview' to run your code
 });
