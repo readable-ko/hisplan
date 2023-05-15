1e865777c02f82c1d4c6cf43075c740c9e5db9a6
Yongseok-Lee <n111224339@gmail.com>
2023-05-15 17:02:09 +0900

    Modify SQL Engine

--- src/pages/GroupMatching.daaxr.js
+++ src/pages/GroupMatching.daaxr.js
@@ -1,54 +1,15 @@
-import wixData from "wix-data";
-import { sql } from "@velo/wix-data-sql-backend";
+import { sql } from "backend/exposer.jsw";
 // import { getStudentInfo, groupMatching } from "backend/groupMatchingModule.jsw";
 
 $w.onReady(function () {
   $w("#button").onClick(async () => {
-    await wixData
-      .query("Student")
-      .find()
-      .then((results) => {
-        if (results.items.length > 0) {
-          // let students =
-          // getStudentInfo(results.items);
-          // console.log(students);
-          // let studyGroups = groupMatching(students);
-          // console.log(studyGroups);
-        } else {
-          $w("#responseText").text = "No students";
-        }
-      })
-      .catch((error) => {
-        console.log(error);
-      });
+    try {
+      const results = await sql("SELECT * FROM Student");
+      console.log(results.payload.data);
+    } catch (error) {
+      console.log(error);
+    }
   });
 });
 
-function getStudentInfo(items) {
-  // let students = [];
-
-  for (let i = 0; i < items.length; ++i) {
-    wixData
-      .query("Preference")
-      .eq("student", items[i]._id)
-      .find()
-      .then((results) => {
-        if (results.items.length > 0) {
-          let preferences = results.items;
-        } else {
-          $w("#responseText").text = "No preferences";
-        }
-      });
-
-    // let student = {
-    //   id: items[i].studentId,
-    //   friends: preference.friends,
-    //   first: preference.first,
-    //   second: preference.second,
-    //   third: preference.third,
-    // };
-    // students.push(student);
-  }
-
-  // return students;
-}
+function getStudentInfo(items) {}
