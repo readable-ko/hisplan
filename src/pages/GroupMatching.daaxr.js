68acb4bdf59ac7ad71451834e9425969e531473e
Suhyun Rim <ish000118@gmail.com>
2023-05-27 14:34:54 +0900

    Update GroupMatching.daaxr.js

--- src/pages/GroupMatching.daaxr.js
+++ src/pages/GroupMatching.daaxr.js
@@ -1,12 +1,4 @@
 import { matchGroups } from "backend/groupMatchingModule.jsw";
-import { $w } from 'wix-sdk';
-
-$w.onReady(() => {
-  $w('#dropdownFriends').onChange(function () {
-    const selectedOption = $w('#dropdownFriends').value;
-    $w('#textboxFriends').value = selectedOption;
-  });
-});
 
 $w.onReady(() => {
   $w("#button").onClick(async () => {
