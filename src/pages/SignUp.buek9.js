1ead7afad8504c185f4aa5ca980382f6cc5d7ef2
KIMHYUNUK98 <21800201@handong.edu>
2023-05-27 22:09:01 +0900

    haha

--- src/pages/SignUp.buek9.js
+++ src/pages/SignUp.buek9.js
@@ -5,7 +5,7 @@ $w.onReady(() => {
     // wixData.query('LoginedEmail').find().then(results => {
     //     console.log(results.items[0]['sharedemail']);
     //     firstEmail = results.items[0]['sharedemail'];
-    // });
+    // });  
 
     $w("#button1").onClick(async () => {
         try {
