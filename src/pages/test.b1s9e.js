fa56de1bd14fe5b088871e4d0ae702fbfb8fd885
Suhyun Rim <ish000118@gmail.com>
2023-05-27 16:56:34 +0900

    Update test.b1s9e.js

--- src/pages/test.b1s9e.js
+++ src/pages/test.b1s9e.js
@@ -9,9 +9,6 @@ $w.onReady(function () {
     const selectedOption = $w('#dropdown1').value;
     $w('#textBox1').value += selectedOption;
     $w('#textBox1').value += " ";
-    
-    // 선택한 옵션을 배열에 추가
-    selectedOptions.push( selectedOption );
   });
   
   $w("#button1").onClick(async () => {
@@ -19,17 +16,15 @@ $w.onReady(function () {
     console.log($w('#textBox1').value);
     try {
       // 신규 문서 추가
-      const newDocument = await wixData.insert("Preference", { test1: $w('#textBox1').value });
+      const newDocument = await wixData.insert("Preference", { friends: $w('#textBox1').value });
       const documentId = newDocument._id; // 신규 문서의 ID 얻기
       
       // 데이터베이스 업데이트
       const result = await wixData.update("Preference", documentId, {
-        test1: $w('#textBox1').value
+        friends: $w('#textBox1').value
       });
       console.log("Data updated successfully:", result);
       
-      // 배열 초기화
-      selectedOptions = [];
     } catch (error) {
       console.error("Failed to update data:", error);
     }
