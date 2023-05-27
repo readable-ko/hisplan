925a973a08995eaae67b85fe293797387e8c2fb4
고영광 <70497430+readable-ko@users.noreply.github.com>
2023-05-27 15:31:46 +0900

    Create test.b1s9e.js

---
+++ src/pages/test.b1s9e.js
@@ -0,0 +1,15 @@
+// Velo API 레퍼런스: https://www.wix.com/velo/reference/api-overview/introduction
+
+$w.onReady(function () {
+
+	// Velo 프레임워크 API를 사용하여 자바스크립트 코드를 입력하세요.
+  console.log("Connected!");
+	// Hello world 출력:
+	// console.log("Hello world!");
+
+	// 페이지 요소에 함수를 요청하세요. 예:
+	// $w("#button1").label = "Click me!";
+
+	// "실행"을 클릭하거나 사이트를 미리보기하여 코드를 실행하세요.
+
+});
