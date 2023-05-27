62c25071db022d60f46167f8e17cf7595dbbf889
okkyun <54517729+wok1909@users.noreply.github.com>
2023-05-27 14:42:50 +0900

    Update GroupMatching.daaxr.js

--- src/pages/GroupMatching.daaxr.js
+++ src/pages/GroupMatching.daaxr.js
@@ -1,6 +1,30 @@
 import { matchGroups } from "backend/groupMatchingModule.jsw";
 
 $w.onReady(() => {
+  
+  // Dropbox JavaScript SDK 로드
+  const Dropbox = require('dropbox').Dropbox;
+  const dbx = new Dropbox("#dropdown1");
+
+  // column 정보 가져오기
+  dbx.filesListFolder("Course")
+    .then(response => {
+      const columns = response.entries; // column 정보를 가져옵니다.
+  
+      // 두 개의 column 정보를 합치기
+      const combinedColumns = columns.map(column => {
+        return column.subject + ' ' + column.courseId;
+      });
+
+      // 합쳐진 column 정보를 표시
+      combinedColumns.forEach(column => {
+        console.log(column);
+      });
+    })
+    .catch(error => {
+      console.error(error);
+    });
+  
   $w("#button").onClick(async () => {
     try {
       const studyGroups = await matchGroups();
