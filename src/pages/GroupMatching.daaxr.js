50f74cea957d17525c514b4f7fef0e90d4efb7f2
KIMHYUNUK98 <21800201@handong.edu>
2023-05-27 23:03:54 +0900

    haha

--- src/pages/GroupMatching.daaxr.js
+++ src/pages/GroupMatching.daaxr.js
@@ -7,7 +7,7 @@ $w.onReady(() => {
   console.log(wixUsers.currentUser['id']);
 
   wixData.query('Student').eq('_id', '5ba9ad7b-21ae-4379-b35d-b2dc86e2ccd9').find().then(results => {
-    console.log(results.items);
+    console.log(results.items.map(item => item.studentId));
   })
   
   // 1번 Drop Down 누른 경우
