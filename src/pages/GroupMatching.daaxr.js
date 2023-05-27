4e6363adc8df4b1014822fce86c352b39a7ac6f7
okkyun <54517729+wok1909@users.noreply.github.com>
2023-05-27 16:49:56 +0900

    Update GroupMatching.daaxr.js

--- src/pages/GroupMatching.daaxr.js
+++ src/pages/GroupMatching.daaxr.js
@@ -2,22 +2,26 @@ import { matchGroups } from "backend/groupMatchingModule.jsw";
 
 $w.onReady(() => {
   
-  $w('#dropdown1').onChange(async() => {
-    const selectedvalue1 = $w('#dropdown1').value;
-    $w('#selectionTags1').value = selectedvalue1;
-    const selectedvalue2 = $w('#dropdown2').value;
-    $w('#input2').value = selectedvalue2;
-    const selectedvalue3 = $w('#dropdown3').value;
-    $w('#input3').value = selectedvalue3;
-    console.log(selectedvalue);
-  });
+//   $w('#dropdown1').onChange(async() => {
+//     const selectedvalue1 = $w('#dropdown1').value;
+//     $w('#selectionTags1').value = selectedvalue1;
+//     const selectedvalue2 = $w('#dropdown2').value;
+//     $w('#input2').value = selectedvalue2;
+//     const selectedvalue3 = $w('#dropdown3').value;
+//     $w('#input3').value = selectedvalue3;
+//     console.log(selectedvalue);
+//   });
   
 
-//   // Dropbox JavaScript SDK 로드
+  // Dropbox JavaScript SDK 로드
 //   const Dropbox = require('dropbox').Dropbox;
 //   const dbx = new Dropbox("#dropdown1");
 
-//   // column 정보 가져오기
+  // column 정보 가져오기
+  $w('#dropdown1').onClick(async()) => {
+    const selectedvalue1 = $w('#dropdown1').value;
+    console.log(selectedvalue1);
+  } 
 //   dbx.filesListFolder("Course")
 //     .then(response => {
 //       const columns = response.entries; // column 정보를 가져옵니다.
