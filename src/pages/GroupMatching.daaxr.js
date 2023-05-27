447b91a915cf6c8bded5f85ebeb3b1da28eaf876
KIMHYUNUK98 <21800201@handong.edu>
2023-05-27 19:14:07 +0900

    haha

--- src/pages/GroupMatching.daaxr.js
+++ src/pages/GroupMatching.daaxr.js
@@ -6,31 +6,10 @@ $w.onReady(() => {
   $w('#dropdown1').onChange(async() => {
     const selectedvalue1 = $w('#dropdown1').value;
 
-    
-
     wixData.query('Course').eq('subject', selectedvalue1).find().then(results => {
       let optionvalue = results.items.map(subject => ({label : subject.courseId, value : subject._id}));
       $w('#dropdown4').options = optionvalue;
     })
-
-    const selectedTest = $w('#dropdown4').value;
-
-    console.log('Test = ', selectedTest);
-
-    wixData.query('Course')
-    .ascending('courseId')
-    .eq('_id', selectedTest)
-    .find()
-    .then(results => {
-
-      if (results.items.length > 0) {
-        const result = results.items[0].courseId;
-        console.log('result = ', result);
-      } else {
-        console.log('No results found.');
-      }
-    });
-
   });
 
   $w('#dropdown2').onChange(async() => {
@@ -48,22 +27,15 @@ $w.onReady(() => {
     wixData.query('Course').eq('subject', selectedvalue3).find().then(results => {
       let optionvalue = results.items.map(subject => ({label : subject.courseId, value : subject._id}));
       $w('#dropdown6').options = optionvalue;
-      // console.log($w('#dropdown6').options.map((item) => item.label))
     })
   });
-
-  $w('#dropdown6').onChange(async() => {
-    // console.log($w('#dropdown6').options.map((item) => item.label))
-    // console.log($w('#dropdown6').value)
-    console.log($w('#dropdown6').options[$w('#dropdown6').selectedIndex].label);
-  })
 
   $w("#button1").onClick(async () => {
     try{
       wixData.insert("Preference", { 
-        first: $w('#dropdown4').key, 
-        second: $w('#dropdown5').key, 
-        third: $w('#dropdown6').key
+        first: $w('#dropdown4').options[$w('#dropdown4').selectedIndex].label, 
+        second: $w('#dropdown5').options[$w('#dropdown5').selectedIndex].label, 
+        third: $w('#dropdown6').options[$w('#dropdown6').selectedIndex].label
   });
     } catch{
       console.error("Failed to update data:");
