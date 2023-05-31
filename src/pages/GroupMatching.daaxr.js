4a8195f06e47d850289bfa50a0201c847bd9f4e5
Yongseok-Lee <n111224339@gmail.com>
2023-05-31 15:44:33 +0900

    Update GroupMatching.daaxr.js and delete MyPage.df66s.js

--- src/pages/GroupMatching.daaxr.js
+++ src/pages/GroupMatching.daaxr.js
@@ -7,8 +7,8 @@ $w.onReady(() => {
   // 어느 페이지에서든 자신의 학번을 불러올 수 있는 코드
   let userId;
   wixUsers.currentUser["id"];
-  console.log(wixUsers.currentUser["id"]);
-  console.log("67561d24-e9f2-4d04-a048-e67d44ddd2a9");
+  // console.log(wixUsers.currentUser["id"]);
+  // console.log("67561d24-e9f2-4d04-a048-e67d44ddd2a9");
 
   wixData
     .query("PrivateMembersData")
@@ -36,55 +36,55 @@ $w.onReady(() => {
   }
 
   // 1번 Drop Down 누른 경우
-  $w("#dropdown1").onChange(async () => {
-    const selectedvalue1 = $w("#dropdown1").value;
+  $w("#dropdownSubject1").onChange(async () => {
+    const selectedValue1 = $w("#dropdownSubject1").value;
 
     wixData
       .query("Course")
-      .eq("subject", selectedvalue1)
+      .eq("subject", selectedValue1)
       .find()
       .then((results) => {
-        let optionvalue = results.items.map((subject) => ({ label: subject.courseId, value: subject._id }));
-        $w("#dropdown4").options = optionvalue;
+        const optionValues = results.items.map((subject) => ({ label: subject.instructor, value: subject.courseId }));
+        $w("#dropdownInstructor1").options = optionValues;
       });
   });
 
   // 2번 Drop Down 누른 경우
-  $w("#dropdown2").onChange(async () => {
-    const selectedvalue2 = $w("#dropdown2").value;
+  $w("#dropdownSubject2").onChange(async () => {
+    const selectedValue2 = $w("#dropdownSubject2").value;
 
     wixData
       .query("Course")
-      .eq("subject", selectedvalue2)
+      .eq("subject", selectedValue2)
       .find()
       .then((results) => {
-        let optionvalue = results.items.map((subject) => ({ label: subject.courseId, value: subject._id }));
-        $w("#dropdown5").options = optionvalue;
+        const optionValues = results.items.map((subject) => ({ label: subject.instructor, value: subject.courseId }));
+        $w("#dropdownInstructor2").options = optionValues;
       });
   });
 
   // 3번 Drop Down 누른 경우
-  $w("#dropdown3").onChange(async () => {
-    const selectedvalue3 = $w("#dropdown3").value;
+  $w("#dropdownSubject3").onChange(async () => {
+    const selectedValue3 = $w("#dropdownSubject3").value;
 
     wixData
       .query("Course")
-      .eq("subject", selectedvalue3)
+      .eq("subject", selectedValue3)
       .find()
       .then((results) => {
-        let optionvalue = results.items.map((subject) => ({ label: subject.courseId, value: subject._id }));
-        $w("#dropdown6").options = optionvalue;
+        const optionValues = results.items.map((subject) => ({ label: subject.instructor, value: subject.courseId }));
+        $w("#dropdownInstructor3").options = optionValues;
       });
   });
 
   // Submit 버튼 누른 경우
-  $w("#button1").onClick(async () => {
+  $w("#buttonSubmit").onClick(async () => {
     try {
       wixData.insert("Preference", {
         studentId: userId,
-        first: $w("#dropdown4").options[$w("#dropdown4").selectedIndex].label,
-        second: $w("#dropdown5").options[$w("#dropdown5").selectedIndex].label,
-        third: $w("#dropdown6").options[$w("#dropdown6").selectedIndex].label,
+        first: $w("#dropdownInstructor1").options[$w("#dropdownInstructor1").selectedIndex].value,
+        second: $w("#dropdownInstructor2").options[$w("#dropdownInstructor2").selectedIndex].value,
+        third: $w("#dropdownInstructor3").options[$w("#dropdownInstructor3").selectedIndex].value,
       });
     } catch {
       console.error("Failed to update data:");
