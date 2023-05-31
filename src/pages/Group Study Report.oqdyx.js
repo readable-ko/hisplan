d2b01d6638e9c5b86e4ac549d254b7cbb22f18fd
okkyun <54517729+wok1909@users.noreply.github.com>
2023-05-31 18:13:31 +0900

    Update Group Study Report.oqdyx.js

--- src/pages/Group Study Report.oqdyx.js
+++ src/pages/Group Study Report.oqdyx.js
@@ -1,5 +1,6 @@
 // API Reference: https://www.wix.com/velo/reference/api-overview/introduction
 // “Hello, World!” Example: https://learn-code.wix.com/en/article/1-hello-world
+import { getGroup } from "backend/data";
 import wixWindow from "wix-window";
 import wixData from "wix-data";
 import { currentMember } from "wix-members";
