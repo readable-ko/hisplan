2499817cf9c02e66ffb9051a903848672674b6b2
okkyun <54517729+wok1909@users.noreply.github.com>
2023-05-31 20:06:28 +0900

    Update Group Study Report.oqdyx.js

--- src/pages/Group Study Report.oqdyx.js
+++ src/pages/Group Study Report.oqdyx.js
@@ -40,12 +40,12 @@ $w.onReady(function () {
     console.log(randomStr);
     console.log(visitorId);
     
-//     const groupNum = getGroup(visitorEmail);
-//     console.log('group num ', groupNum);
+    const groupNum = getGroup(visitorEmail);
+    console.log('group num ', groupNum.items);
     
-    getGroup(visitorEmail).then((items) => {
-      console.log("Returned items: ", items);
-    });
+//     getGroup(visitorEmail).then((items) => {
+//       console.log("Returned items: ", items);
+//     });
 
     
   });
