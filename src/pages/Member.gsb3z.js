097f9901aebd52e1644ba431c33244c8a2df7a5b
okkyun <54517729+wok1909@users.noreply.github.com>
2023-06-02 20:12:26 +0900

    Update Member.gsb3z.js

--- src/pages/Member.gsb3z.js
+++ src/pages/Member.gsb3z.js
@@ -18,18 +18,18 @@ $w.onReady(async () => {
   let members = groupMembers.items[0].members;
   let n_members = members.length;
   
-  $w("#repeater1").onItemReady( ($w, itemData, index) => {
+  $w("#repeater1").onItemReady( async ($w, itemData, index) => {
     // Repeater의 각 아이템에서 text1의 정보를 가져와서 출력
     
     const name = itemData.name;  // text1 필드의 값을 가져옴
     const studentId = itemData.studentId;  // text1 필드의 값을 가져옴
     const email = itemData.email;
     
-//     let img = await getProfileImage(email);
+    let img = await getProfileImage(email);
     
     $w("#text3").text = name;  // text1 요소에 가져온 값을 설정하여 출력
     $w("#text4").text = studentId;  // text1 요소에 가져온 값을 설정하여 출력
-//     $w("#imageX3").src = img;
+    $w("#imageX3").src = img;
     
     console.log(name, studentId, email);  // 콘솔에 출력하거나 원하는 작업 수행
   });
