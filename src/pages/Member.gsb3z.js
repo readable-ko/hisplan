9d584aea316e855e600574fea4f2772eb6b1fdfd
okkyun <54517729+wok1909@users.noreply.github.com>
2023-06-02 18:16:53 +0900

    Update Member.gsb3z.js

--- src/pages/Member.gsb3z.js
+++ src/pages/Member.gsb3z.js
@@ -1,7 +1,7 @@
 // API Reference: https://www.wix.com/velo/reference/api-overview/introduction
 // “Hello, World!” Example: https://learn-code.wix.com/en/article/1-hello-world
 import wixData from "wix-data";
-import { currentMember } from "wix-members";
+import { currentMember, getGroupMembers } from "wix-members";
 import { getGroup } from "backend/data";
 let visitorId;
 let visitorEmail;
