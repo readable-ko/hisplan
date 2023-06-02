e59597fd884b6c8ccd0627b275cd01f63fed3e25
okkyun <54517729+wok1909@users.noreply.github.com>
2023-06-02 20:02:33 +0900

    Update Member.gsb3z.js

--- src/pages/Member.gsb3z.js
+++ src/pages/Member.gsb3z.js
@@ -18,7 +18,7 @@ $w.onReady(async () => {
   let members = groupMembers.items[0].members;
   let n_members = members.length;
   
-  let image = getProfileImage(visitorEmail);
+  let image = await getProfileImage(visitorEmail);
   $w("#imageX4").src = image;
   
   $w("#repeater1").onItemReady( ($w, itemData, index) => {
