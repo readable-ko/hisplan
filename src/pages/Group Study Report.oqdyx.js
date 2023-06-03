323b98124efc46a6bdaa94b594927c4130992d3f
Suhyun Rim <ish000118@gmail.com>
2023-06-03 17:07:44 +0900

    Update Group Study Report.oqdyx.js

--- src/pages/Group Study Report.oqdyx.js
+++ src/pages/Group Study Report.oqdyx.js
@@ -1,6 +1,7 @@
 // API Reference: https://www.wix.com/velo/reference/api-overview/introduction
 // “Hello, World!” Example: https://learn-code.wix.com/en/article/1-hello-world
-import { getGroup, getGroupMembers, getStudentGroup, increaseGroupReport, getStudentID } from "backend/data";
+// import { getGroup, getGroupMembers, getStudentGroup, increaseGroupReport, getStudentID } from "backend/data";
+import { getGroup, getGroupMembers, getProfileImage } from "backend/data";
 import wixWindow from "wix-window";
 import wixData from "wix-data";
 import { currentMember } from "wix-members";
